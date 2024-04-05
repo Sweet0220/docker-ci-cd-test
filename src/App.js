f9c45eb31191c2260d0fc35aced05f6709f05200
Nealcos Mircea <nealcos.va.mircea@student.utcluj.ro>
2024-04-05 16:44:14 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I was changed!QQ
         </p>
         <a
           className="App-link"
